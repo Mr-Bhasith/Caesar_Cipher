e17b2e7dfd4711c79cdda67d674ea9d990eadc1e
Mr-Bhasith <thamisbhasith8@gmail.com>
2021-05-27 11:58:49 +0530

    Update Caesar Cipher.py

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -1,35 +1,71 @@
-import string
+#!/usr/bin/python3
 
-word = "Hello World"
-cipher_word = ""
-shift = 2
+from argparse import ArgumentParser
 
-for char in word:
-	if char == ' ':
-		cipher_word += char
-	elif char.isupper():
-		cipher_word += chr((ord(char)+shift-65)%26+65)
-	else:
-		cipher_word += chr((ord(char)+shift-97)%26+97)
 
-# def caesar(text, shift, alphabets):
-# 	def shift_alpha(alphabet):
-# 		return alphabet[shift:] + alphabet[:shift]
+class Cipher:
+    def __init__(self):
+        if __name__ == "__main__":
+            parser = ArgumentParser(
+                description="Caeser Cipher is a method of Encrypt or Decrypt a message")
+            parser.add_argument(
+                "crypt",
+                type=str,
+                help="Give 'encrypt' if you want to encrypt a message or Give 'decrypt' if you want to decrypt a message")
 
-# 	shifted_alphas = tuple(map(shift_alpha, alphabets))
-# 	final_alphabet = ''.join(alphabets)
-# 	final_shifted_alpha = ''.join(shifted_alphas)
-# 	table = str.maketrans(final_alphabet,final_shifted_alpha)
-# 	print(alphabets)
-# 	print(shifted_alphas)
-# 	print(final_alphabet)
-# 	print(final_shifted_alpha)
-# 	print(table)
-# 	return text.translate(table)
+            args = parser.parse_args()
 
-# shifted = 2
-# # shifted = 26-2
-# # shifted %= 26
+            final_message = None
 
-# print(caesar(word, shifted, [string.ascii_lowercase, string.ascii_uppercase]))
-print(cipher_word)
+            try:
+                if args.crypt == "encrypt":
+                    message, shift = self.secretMessage()
+                    final_message = self.encrypt(message, shift)
+
+                    print("\nEncrypted Message: ", final_message)
+                elif args.crypt == "decrypt":
+                    message, shift = self.secretMessage()
+                    final_message = self.decrypt(message, shift)
+
+                    print("\nDecrypted Message: ", final_message)
+                else:
+                    print("Invalid argument!")
+            except ValueError:
+                print("\nThe shift value should be a number!")
+            except KeyboardInterrupt:
+                print("\nCtrl + c detected!")
+
+    def secretMessage(self):
+        message = input("Enter the message: ")
+        shift = int(input("Enter the shift key: "))
+
+        return message, shift
+
+    def encrypt(self, message, shift):
+        encrypted_message = ""
+
+        for char in message:
+            if char == " ":
+                encrypted_message += char
+            elif char.isupper():
+                encrypted_message += chr((ord(char) + shift - 65) % 26 + 65)
+            else:
+                encrypted_message += chr((ord(char) + shift - 97) % 26 + 97)
+
+        return encrypted_message
+
+    def decrypt(self, message, shift):
+        decrypted_message = ""
+
+        for char in message:
+            if char == " ":
+                decrypted_message += char
+            elif char.isupper():
+                decrypted_message += chr((ord(char) - shift - 65) % 26 + 65)
+            else:
+                decrypted_message += chr((ord(char) - shift - 97) % 26 + 97)
+
+        return decrypted_message
+
+
+Cipher()
